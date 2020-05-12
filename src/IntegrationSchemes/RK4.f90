ed59f70d4735a555664edef2233e8b8f764c5be5
marco1410 <mzuniga433@alumnos.iua.edu.ar>
2020-05-12 15:10:05 -0300

    andando

--- src/IntegrationSchemes/RK4.f90
+++ src/IntegrationSchemes/RK4.f90
@@ -33,16 +33,12 @@ contains
     class(IntegrandDT), allocatable        :: k4
     select type (this)
     class is (IntegrandDT)
-       allocate(v1(size(this%state)))
-       allocate(v2(size(this%state)))
-       allocate(v3(size(this%state)))
-       allocate(v4(size(this%state)))
        allocate(k1, source = this)
        allocate(k2, source = this)
        allocate(k3, source = this)
        allocate(k4, source = this)
        v1 = this%state
-       k1 = k1%t(v1)
+       k1 = this%t(v1)
        v2 = this%state+0.5*dt*k1%state
        k2 = this%t(v2)
        v3 = this%state+0.5*dt*k2%state
@@ -51,7 +47,7 @@ contains
        k4 = this%t(v4)
        this=this&
             +(k1+k2*2.d0+k3*2.d0+k4)*(1/6.d0)*dt
-       deallocate(k1,k2,k3,k4,v1,v2,v3,v4)
+       deallocate(k1,k2,k3,k4)
     class default
        stop 'integrate: unsupported class'
     end select
